f70895a130d6859dabe145d1c31a006d9c3bffee
Levin Jian <jianzhirong@gmail.com>
2017-06-25 16:37:39 +0800

    extract type

--- fieldlocalization.py
+++ fieldlocalization.py
@@ -167,6 +167,93 @@ class FieldLocation(Findlines):
         self.field_name = field
          
         return
+    def find_type(self, region):
+
+        field = region[:, 640:1230]
+        
+        #downsample and use it for processing
+        self.rgb = cv2.pyrDown(field);
+        self.small = cv2.cvtColor(self.rgb,  cv2.COLOR_BGR2GRAY);
+#         self.small = cv2.Sobel(self.small, cv2.CV_8U, 1, 0)
+#         channels = cv2.cvtColor(self.rgb, cv2.COLOR_BGR2Lab)
+#         self.small = channels[:,:,0]
+        #self.small = cv2.cvtColor(self.rgb,  cv2.COLOR_BGR2GRAY);
+        
+#         kernel = np.ones((3,3),np.uint8)
+#         self.small = cv2.erode(self.small,kernel,iterations = 1)
+        
+        #morphological gradient
+        morphKernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3));
+        self.grad = cv2.morphologyEx(self.small, cv2.MORPH_GRADIENT, morphKernel);
+        
+        #binarize
+    
+        thres, self.bw = cv2.threshold(self.grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU);
+        
+        #Remvoe noise around edge
+        self.erosion = self.bw.copy()
+        pad = 7
+        self.erosion = self.bw.copy()
+        self.erosion[:,0:(pad)] = 0
+        self.erosion[:,-pad:] = 0
+        self.erosion[0:(pad), :] = 0
+        self.erosion[-(pad):, :] = 0
+        
+        
+        #connect horizontally oriented regions
+        morphKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1));
+        self.connected = cv2.morphologyEx(self.erosion, cv2.MORPH_CLOSE, morphKernel);
+        
+#         self.find_rec(self.erosion)
+#          
+#          
+#          
+#         #find contours
+        im2, contours, hierarchy = cv2.findContours(self.connected.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+          
+          
+        self.res = self.rgb.copy()
+        mask = np.zeros(self.rgb.shape[:2], np.uint8)
+        self.contour = self.rgb.copy()
+          
+        # filter contours
+        x_top = []
+        y_top = []
+        x_bottom = []
+        y_bottom = []
+        for idx in range(0, len(contours)):
+            x, y, rect_width, rect_height = cv2.boundingRect(contours[idx])
+            if rect_height < 20 or rect_width < 3:
+                continue
+            if (rect_width < 20 and x < 15):
+                continue
+            cv2.drawContours(mask, contours, idx, 255, cv2.FILLED)
+            cv2.drawContours(self.contour, contours, idx, (0,0,255))
+            
+            x_top.append(x)
+            y_top.append(y)
+            x_bottom.append(x + rect_width)
+            y_bottom.append(y + rect_height)
+        
+        if len(x_top) != 0:  
+            x_top, y_top, x_bottom, y_bottom = self.adjust_rect(np.min(x_top), np.min(y_top), np.max(x_bottom), np.max(y_bottom), pad, self.rgb.shape[:2])
+            cv2.rectangle(self.res, (x_top, y_top ), (x_bottom, y_bottom ), (0,255,0),1)
+          
+          
+        self.field_name = field
+         
+        return
+    def adjust_rect(self, x_top, y_top, x_bottom, y_bottom, pad, shape):
+        height, width = shape
+        if x_top == pad:
+            x_top = 0
+        if y_top == pad:
+            y_top = 0
+        if x_bottom == width - pad:
+            x_bottom = width
+        if y_bottom == height -pad:
+            y_bottom = height
+        return x_top, y_top, x_bottom, y_bottom
     def find_names(self, region):
         field_pos = 380
         field = self.fields[:, 65:380]
@@ -242,7 +329,7 @@ class FieldLocation(Findlines):
             if r > 0.01:
                
 #                 cv2.drawContours(self.res, contours, idx, (0, 255, 0))
-                cv2.rectangle(self.res, (x, y+rect_height), (x+rect_width, y), (0,255,0),3)
+                cv2.rectangle(self.res, (x, y+rect_height), (x+rect_width, y), (0,255,0),1)
           
           
         self.field_name = field
@@ -254,8 +341,9 @@ class FieldLocation(Findlines):
         self.fields = hough_lines.copy()
         
         self.hough_lines = hough_lines
-        self.find_names(hough_lines.copy())
-        self.find_sex(hough_lines.copy())
+#         self.find_names(hough_lines.copy())
+#         self.find_sex(hough_lines.copy())
+        self.find_type(hough_lines.copy())
         
 #         name_pos = 380
 #         sex_pos = 560
@@ -275,7 +363,7 @@ class FieldLocation(Findlines):
     def save_all_fields(self):
         expl = ExploreImages()
         fnames = expl.get_all_imags()
-        region_folder = './data/fields/sex/'
+        region_folder = './data/fields/type/'
         if not os.path.exists(region_folder):
             os.makedirs(region_folder)
         count = 0
@@ -291,9 +379,9 @@ class FieldLocation(Findlines):
     
     def run(self):
         
-#         return self.save_all_fields()
+        return self.save_all_fields()
         
-        fnames = ['./data/ocr/00000016AI20160325010.jpg']
+        fnames = ['./data/ocr/00000013AI20160329001.jpg']
 
         
 #         fnames = ['./data/ocr/00000012AI20160328023.jpg','./data/ocr/00000015AI20160328023.jpg',
@@ -310,8 +398,8 @@ class FieldLocation(Findlines):
             print("image {}".format(fname))
             self.find_fields(fname, raise_exception = False)
 #             self.show_channels(fname)
-#             res_imgs.append(self.stack_image_horizontal([self.hough_lines, self.field_name,self.grad, self.connected, self.res]))
-            res_imgs.append(self.stack_image_horizontal([self.small,  self.grad, self.bw, self.erosion,self.connected, self.contour, self.res]))
+            res_imgs.append(self.stack_image_horizontal([self.res]))
+#             res_imgs.append(self.stack_image_horizontal([self.small,  self.grad, self.bw, self.erosion,self.connected, self.contour, self.res]))
            
         
         res_imgs = self.stack_image_vertical(res_imgs)
